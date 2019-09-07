3bc6b03450a6d2a15e6ab40ccc4247cb3006e97b
Steve Dougherty <steve@asksteved.com>
2019-09-07 09:00:15 -0400

    Move network operation to thread
    This also prevents starting the coroutine more than once. Logging seems to work
    fine still.

--- Song Refresh Button BSIPA/MedicorePuller.cs
+++ Song Refresh Button BSIPA/MedicorePuller.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using IPA.Utilities;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,7 +16,11 @@ namespace Song_Refresh_Button_BSIPA
     public class MedicorePuller : MonoBehaviour
     {
         public static MedicorePuller Instance;
+
+        private static bool _runningPull;
         private static bool _conversionDone;
+
+        private static string[] _components;
 
         public static void OnLoad()
         {
@@ -29,29 +35,92 @@ namespace Song_Refresh_Button_BSIPA
         private void Awake()
         {
             Instance = this;
+            _runningPull = false;
             DontDestroyOnLoad(gameObject);
         }
 
         // TODO: Take MediocreMapper host and port as arguments, pulled from plugin config
         public void Pull()
         {
-            if (SceneManager.GetActiveScene().name != "MenuCore")
+            if (SceneManager.GetActiveScene().name != "MenuCore" || _runningPull)
             {
                 return;
             }
 
-            // TODO: Avoid running more than one at once?
+            _runningPull = true;
             StartCoroutine(PullSong());
         }
 
         private IEnumerator PullSong()
         {
+            try
+            {
+                _components = null;
+                new Thread(delegate(object o) { _components = ReadWelcomeMessage(); }).Start();
+
+                yield return new WaitUntil(() => _components != null);
+
+                var folderName = _components[0].Split(new[] {"::"}, StringSplitOptions.None)[0];
+                var difficultyFilename = _components[2];
+                var difficultyContent = _components[3];
+                Logger.log.Debug($"Folder: {folderName}\nDifficulty: {difficultyFilename}\nDifficulty size: {difficultyContent.Length} characters");
+
+                var customSongsPath = $"{BeatSaber.InstallPath}\\Beat Saber_Data\\CustomWIPLevels";
+                var songPath = $"{customSongsPath}\\{folderName}";
+                var difficultyPath = $"{songPath}\\{difficultyFilename}";
+                Logger.log.Debug($"Writing to {difficultyPath}");
+                using (var outputDifficulty = File.CreateText(difficultyPath))
+                {
+                    outputDifficulty.Write(difficultyContent);
+                }
+                Logger.log.Debug("Wrote file");
+
+                var converterPath = BeatSaber.InstallPath + "\\songe-converter.exe";
+                if (File.Exists(converterPath))
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo();
+                    startInfo.WindowStyle = ProcessWindowStyle.Normal;
+                    startInfo.FileName = converterPath;
+                    startInfo.Arguments = $"-k -a \"{customSongsPath}\"";
+                    startInfo.UseShellExecute = false;
+                    process.StartInfo = startInfo;
+                    process.EnableRaisingEvents = true;
+                    process.Exited += Process_Exited;
+
+                    var stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    process.Start();
+                    Logger.log.Debug($"Started conversion.");
+
+                    yield return new WaitUntil(() => _conversionDone);
+
+                    Logger.log.Debug($"Conversion complete in {stopwatch.Elapsed}; refreshing songs.");
+                    SongCore.Loader.Instance.RefreshSongs();
+                }
+                else
+                {
+                    Logger.log.Info($"{converterPath} does not exit; can't convert.'");
+                    // TODO: Is there a way to exit a coroutine? Will this branch hang?
+                }
+            }
+            finally
+            {
+                _runningPull = false;
+            }
+        }
+
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            // TODO: persist process handle and check exit code?
+            _conversionDone = true;
+        }
+
+        private string[] ReadWelcomeMessage()
+        {
             var stopwatch = new Stopwatch();
 
             // TODO: How to display progress? text requires a transform.
-            // TODO: Thread out for conversion and notify on completion - I'm seeing stuff with coroutines as well.
-            // TODO: Maybe coroutine that waits for non-null message string to set?
-            // TODO: Break into ReadWelcomeMessage()
             Logger.log.Debug("Opening TCP connection");
             stopwatch.Start();
             // TODO: Connection error notification
@@ -71,8 +140,6 @@ namespace Song_Refresh_Button_BSIPA
             //  4: audio filename
             //  5: audio filesize
             //  6: audio download url
-            // TODO: Could optimize to not recheck for ;;; beyond uhhh up to last 2 previous characters (in case ;;; gets split between chunks) doesn't seem remotely worth it.
-            // TODO: will this kill the server if it closes the stream during the welcome message? (it if prevents the welcome message from being entirely sent anyway)
             var message = new StringBuilder();
             var buffer = new byte[4096];
             Logger.log.Debug("Reading welcome message");
@@ -85,57 +152,7 @@ namespace Song_Refresh_Button_BSIPA
             client.Close();
             Logger.log.Debug($"Read welcome message in {stopwatch.Elapsed} seconds");
 
-            // TODO: Go out to thread for network and wait as coroutine? Like with conversion.
-            var components = message.ToString().Split(new[] {";;;"}, StringSplitOptions.None);
-
-            var folderName = components[0].Split(new[] {"::"}, StringSplitOptions.None)[0];
-            var difficultyFilename = components[2];
-            var difficultyContent = components[3];
-            Logger.log.Debug($"Folder: {folderName}\nDifficulty: {difficultyFilename}\nDifficulty size: {difficultyContent.Length} characters");
-
-            var customSongsPath = $"{BeatSaber.InstallPath}\\Beat Saber_Data\\CustomWIPLevels";
-            var songPath = $"{customSongsPath}\\{folderName}";
-            var difficultyPath = $"{songPath}\\{difficultyFilename}";
-            Logger.log.Debug($"Writing to {difficultyPath}");
-            using (var outputDifficulty = File.CreateText(difficultyPath))
-            {
-                outputDifficulty.Write(difficultyContent);
-            }
-            Logger.log.Debug("Wrote file");
-
-            var converterPath = BeatSaber.InstallPath + "\\songe-converter.exe";
-            if (File.Exists(converterPath))
-            {
-                Process process = new Process();
-                ProcessStartInfo startInfo = new ProcessStartInfo();
-                startInfo.WindowStyle = ProcessWindowStyle.Normal;
-                startInfo.FileName = converterPath;
-                startInfo.Arguments = $"-k -a \"{customSongsPath}\"";
-                startInfo.UseShellExecute = false;
-                process.StartInfo = startInfo;
-                process.EnableRaisingEvents = true;
-                process.Exited += Process_Exited;
-
-                stopwatch.Restart();
-                process.Start();
-                Logger.log.Debug($"Started conversion.");
-
-                yield return new WaitUntil(() => _conversionDone);
-
-                Logger.log.Debug($"Conversion complete in {stopwatch.Elapsed}; refreshing songs.");
-                SongCore.Loader.Instance.RefreshSongs();
-            }
-            else
-            {
-                Logger.log.Info($"{converterPath} does not exit; can't convert.'");
-                // TODO: Is there a way to exit a coroutine? Will this branch hang?
-            }
-        }
-
-        private void Process_Exited(object sender, EventArgs e)
-        {
-            // TODO: persist process handle and check exit code?
-            _conversionDone = true;
+            return message.ToString().Split(new[] {";;;"}, StringSplitOptions.None);
         }
     }
 }
