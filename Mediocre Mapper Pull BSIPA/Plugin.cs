2a8b770184d93145c30d14cc58101c488cf459d9
Steve Dougherty <steve@asksteved.com>
2019-09-08 17:30:49 -0400

    Reduce nesting

--- Mediocre Mapper Pull BSIPA/Plugin.cs
+++ Mediocre Mapper Pull BSIPA/Plugin.cs
@@ -39,17 +39,16 @@ namespace Mediocre_Mapper_Pull_BSIPA
 
         public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
         {
-            if (scene.name == "MenuCore")
-            {
-                MediocrePuller.OnLoad();
-                CustomUI.MenuButton.MenuButtonUI.AddButton("MediocreMapper Pull", "Pulls from Mediocre Mapper server",
-                    delegate
-                    {
-                        MediocrePuller.Instance.Pull(
-                            _config.GetString("MMMM", "hostname", "127.0.0.1", true),
-                            _config.GetInt("MMMM", "port", 17425, true));
-                    });
-            }
+            if (scene.name != "MenuCore") return;
+
+            MediocrePuller.OnLoad();
+            CustomUI.MenuButton.MenuButtonUI.AddButton("MediocreMapper Pull", "Pulls from Mediocre Mapper server",
+                delegate
+                {
+                    MediocrePuller.Instance.Pull(
+                        _config.GetString("MMMM", "hostname", "127.0.0.1", true),
+                        _config.GetInt("MMMM", "port", 17425, true));
+                });
         }
 
         public void OnSceneUnloaded(Scene scene)
